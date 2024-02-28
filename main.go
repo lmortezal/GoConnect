b24925975870a8b5de875c47fd9449ade81e17d8
Morteza <lmortezal.world@gmail.com>
2024-02-28 19:49:43 +0330

    Add a flag to main.go

--- main.go
+++ main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/lmortezal/GoConnect/connect"
 )
 
-
-
 func main() {
-	a := os.Args
-	if len(a) == 1{
-		fmt.Println(`this is a Syncer app
-	tempalate for a syncer app is as follows:
-	  ./Goconnect <workdir>(default is ./) <destination>(e.g. User@ServerIp:/path/you/want/sync) <port>(default is 22) `)
+	var (
+		source string
+		destination string
+		port uint
+	)
+	dir , _ := os.Executable()
+	flag.StringVar(&source, "s", dir , "source directory (e.g. /home/user/GoConnect )")
+	flag.StringVar(&destination, "d", "" , "destination directory (e.g. user@golang:/home/target/GoConnect )")
+	flag.UintVar(&port, "p", 22 , "port number")
+	flag.Parse()
+	flag.Func("help", "show help" , func(s string) error { flag.PrintDefaults(); return nil })
+	if len(os.Args) == 1{
+		flag.PrintDefaults()
+		return
+	}else if destination == "" {
+		flag.PrintDefaults()
 		return
 	}
-	connect.Connect(a[1] , a[2] )
+	var destinations [3]string
+	destinations[0] = strings.Split(destination , "@")[0]
+	destinations[2] = strings.Split(destination , ":")[1] 
+	destinations[1] = strings.Split(strings.Split(destination, "@")[1],":")[0]
+	fmt.Println(destinations)
+
+	connect.Connect(source , port , destinations)
+
 
 }
 
