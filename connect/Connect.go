79515270819053efe3cdf79467cc5b7c9c4984f4
Morteza <lmortezal.world@gmail.com>
2024-02-28 15:11:51 +0330

    stuck at ssh and executed command
    i should create a func for command

--- connect/Connect.go
+++ connect/Connect.go
@@ -3,49 +3,37 @@ package connect
 import (
 	"bytes"
 	"fmt"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 	"io/fs"
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"strings"
-
-	"github.com/pkg/sftp"
-	"golang.org/x/crypto/ssh"
 )
+
 var (
 	privateKeyPath string = "/home/morteza/.ssh/id_rsa"
-	Password string
-	ip_ssh string
-	user_ssh string
+	Password       string
+	ip_ssh         string
+	user_ssh       string
 	workdir_client string
 	workdir_target *string
-	session *ssh.Session
+	session        *ssh.Session
 	// protect the password and save it in a file
 )
 
-func error_check(err error , line uint) {
+func loger() {
+	_, _, line, _ := runtime.Caller(1)
+	log.Printf("--%v--\n", line)
+}
+
+func error_check(err error, line uint) {
 	// each error shoud be stop the program
 	if err != nil {
-		log.Println(err , "in line: " , line)
+		log.Println(err, "in line: ", line)
 	}
-}
-
-func Check_Dir(path fs.DirEntry) bool{
-	// check the directory is it dir or not
-	return path.IsDir()
-}
-
-func Chech_hash(file_client,file_target string)  bool{
-
-	// md5_target , err := session.CombinedOutput("md5sum " +  *workdir_target + file_target)
-	md5_target , err := session.Output("ls")
-	error_check(err, 41)
-	fmt.Println(string(md5_target))
-
-	md5_client , err := exec.Command("md5sum", workdir_client + file_client).Output()
-	error_check(err, 43)
-	fmt.Println(string(md5_client) + " client------target " + string(md5_target))
-	return string(md5_client) == string(md5_target) 
 }
 
 func Check_method_connect() *ssh.ClientConfig {
@@ -78,15 +66,55 @@ func Check_method_connect() *ssh.ClientConfig {
 	}
 	return nil
 }
+func Check_Dir(path fs.DirEntry) bool {
+	// check the directory is it dir or not
+	return path.IsDir()
+}
+
+
+func Chech_hash(file_client, file_target string) bool {
+
+	// check the hash of the file
+	// from server check the hash of the file
+	md5_target, _ := session.Output("md5sum " + file_target)
+	loger()
+	fmt.Println(string(md5_target) + " target")
+	md5_client, _ := exec.Command("md5sum", workdir_client+file_client).Output()
+	loger()
+	fmt.Println(strings.Split(string(md5_client)," ")[0] + " client------target " + string(md5_target))
+	return strings.Split(string(md5_client)," ")[0] == string(md5_target)
+}
+
+func GetHash(){
+	//first connect to the server and get the hash of the file
+	config := Check_method_connect()
+	fmt.Println(ip_ssh)
+	client, err := ssh.Dial("tcp", ip_ssh+":22", config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+	defer client.Close()
+	session, err = client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("cd " + *workdir_target + " && ls -ltrh"); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	var _ = strings.Split(b.String(), "\n")
+	fmt.Println(b.String())
+}
+
 
 func GetFile(client *ssh.Client, fileName string) {
 	var sftpClient *sftp.Client
 	sftpClient, err := sftp.NewClient(client)
-	error_check(err,82)
 	defer sftpClient.Close()
 
-
-	exePath , err := os.Executable()
+	exePath, err := os.Executable()
 	if err != nil {
 		log.Println(err)
 		fmt.Println(exePath)
@@ -94,29 +122,31 @@ func GetFile(client *ssh.Client, fileName string) {
 	// os.Chdir(exePath)
 	os.Chdir("/home/morteza/Desktop/GoConnect/")
 	workdir_client = "/home/morteza/Desktop/GoConnect/"
-	list_Dir , err := os.ReadDir(".")
-	error_check(err,95)
-	for _ , Name_of_File := range list_Dir{
-		if Name_of_File.Name() == fileName && !Check_Dir(Name_of_File) && Chech_hash(Name_of_File.Name(),fileName) {
+	list_Dir, _ := os.ReadDir(".")
+	for _, Name_of_File := range list_Dir {
+		if Name_of_File.Name() == fileName && Name_of_File.IsDir() && Chech_hash(Name_of_File.Name(), fileName) {
 			fmt.Println("The file is already exist")
 		}
-	
+		fmt.Printf(`Name_of_File.Name() : %v
+fileName : %v
+!Check_Dir(Name_of_File) : %v
+Chech_hash(Name_of_File.Name() : %v
+` , Name_of_File.Name() , fileName , !Check_Dir(Name_of_File) , Chech_hash(Name_of_File.Name(), fileName)) 
 	}
+	os.Exit(1)
 	fmt.Println("The file is not exist")
-	
-	
-
 
 }
 
-func Connect(sshAddr string , address_from_main string) {
+func Connect(sshAddr string, address_from_main string) {
 	workdir_target = &address_from_main
 	if sshAddr == "" || len(strings.Split(sshAddr, "@")) < 2 {
 		log.Println(`Wrong input!!`)
-		return 
+		return
 	}
 	user_ssh = strings.Split(sshAddr, "@")[0]
 	ip_ssh = strings.Split(strings.Split(sshAddr, "@")[1], ":")[0]
+	GetHash()
 
 	config := Check_method_connect()
 	client, err := ssh.Dial("tcp", ip_ssh+":22", config)
@@ -133,12 +163,11 @@ func Connect(sshAddr string , address_from_main string) {
 		log.Fatal("Failed to create session: ", err)
 	}
 	defer session.Close()
-
 	// Once a Session is created, you can execute a single command on
 	// the remote side using the Run method.
 	var b bytes.Buffer
 	session.Stdout = &b
-	if err := session.Run("cd "+ *workdir_target +" && ls"); err != nil {
+	if err := session.Run("cd " + *workdir_target + " && ls"); err != nil {
 		log.Fatal("Failed to run: " + err.Error())
 	}
 	var list = strings.Split(b.String(), "\n")
@@ -147,10 +176,10 @@ func Connect(sshAddr string , address_from_main string) {
 		if list[i] == "" {
 			continue
 		}
-		GetFile(client ,list[i])
+		GetFile(client, list[i])
+		fmt.Println("----------------" + list[i] + "----------------")
 		fmt.Println(list[i])
 	}
-
 
 	var sftpClient *sftp.Client
 	sftpClient, err = sftp.NewClient(client)
@@ -159,7 +188,7 @@ func Connect(sshAddr string , address_from_main string) {
 	}
 	defer sftpClient.Close()
 	// creata a txt file in the remote server
-	f , err := sftpClient.Create("/home/ubuntu/test.txt")
+	f, err := sftpClient.Create("/home/ubuntu/test.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -168,7 +197,7 @@ func Connect(sshAddr string , address_from_main string) {
 		log.Fatal(err)
 	}
 	f.Close()
-	
+
 	w := sftpClient.Walk("/home/ubuntu/")
 	for w.Step() {
 		if w.Err() != nil {
@@ -182,6 +211,4 @@ func Connect(sshAddr string , address_from_main string) {
 	}
 	// log.Println(fi)
 
-	
 }
-
