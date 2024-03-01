c4a6b90b12842915c014977b846fea16a9883e15
Morteza <lmortezal.world@gmail.com>
2024-03-01 18:52:04 +0330

    Fix Bug.

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
+
 	"github.com/lmortezal/GoConnect/connect"
 )
 
@@ -16,7 +16,7 @@ func main() {
 		port uint
 	)
 	
-	dir , _ := os.Executable()
+	dir , _ := os.Getwd()
 	flag.StringVar(&destination, "d", dir , "destination directory (e.g. /home/user/GoConnect )")
 	flag.StringVar(&source, "s", "" , "source directory (e.g. user@golang:/home/target/GoConnect )")
 	flag.UintVar(&port, "p", 22 , "port number")
@@ -33,7 +33,6 @@ func main() {
 	sources[0] = strings.Split(source , "@")[0]
 	sources[2] = strings.Split(source , ":")[1] 
 	sources[1] = strings.Split(strings.Split(source, "@")[1],":")[0]
-	fmt.Println(sources)
 
 	connect.Connect(destination , strconv.Itoa(int(port)) , sources)
 
